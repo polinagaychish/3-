6094930ed69481ab1efc75673abb70b272c70f79
Polina Gaychish <polina.gaychish@gmail.com>
2023-05-07 19:38:38 +0300

    work003

---
+++ Homework003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел  от 1 до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+Console.Write($"{Math.Pow(count,3)}");
+count++;
+while (count <=N)
+{
+    double result = Math.Pow(count, 3);
+Console.Write($", {result}");
+count++;
+}
